8ebed29c44a77be41c1a67272dc7e2573b5029bf
smcmanis82 <sharon.mcmanis@gmail.com>
2021-10-13 19:33:38 -0500

    add client and ssl options

--- db/client.js
+++ db/client.js
@@ -2,7 +2,12 @@ const { Client } = require('pg');
 const DB_NAME = 'localhost:5432/charcuterie';
 const DB_URL =
   `${process.env.DATABASE_URL}?sslmode=require` || `postgres://${DB_NAME}`;
-const client = new Client(DB_URL);
+const client = new Client({
+  connectionString: DB_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 module.exports = {
   client,
